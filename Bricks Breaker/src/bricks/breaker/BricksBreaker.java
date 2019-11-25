051d13aacc824765b7d777e34cb1eb3db706b84f
Gracielo Justine <gracielo1@mhs.stts.edu>
2019-11-25 18:08:07 +0700

    coba

--- Bricks Breaker/src/bricks/breaker/BricksBreaker.java
+++ Bricks Breaker/src/bricks/breaker/BricksBreaker.java
@@ -1,7 +1,6 @@
 package bricks.breaker;
 public class BricksBreaker {
     public static void main(String[] args) {
-        From game = new From();
-        game.show();
+        new From().setVisible(true);
     }
 }
