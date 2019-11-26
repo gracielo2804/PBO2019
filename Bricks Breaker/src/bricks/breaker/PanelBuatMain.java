51d1b56102cadf3dcf3da2495137053638d0239b
Gracielo Justine <gracielo1@mhs.stts.edu>
2019-11-26 14:45:46 +0700

    Sudah insert bola ,belum mantul ketika kena brick sduah insert brick

--- Bricks Breaker/src/bricks/breaker/PanelBuatMain.java
+++ Bricks Breaker/src/bricks/breaker/PanelBuatMain.java
@@ -5,15 +5,22 @@
  */
 package bricks.breaker;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.Timer;
 
 /**
  *
@@ -25,14 +32,62 @@ public class PanelBuatMain extends javax.swing.JPanel {
      * Creates new form PanelBuatMain
      */
     int x=(this.getWidth()/2);
+    ImageIcon ballcon=new ImageIcon("Ball.jpg"); 
+    Image ball;
+    int xbola=100,ybola=100,movexbola=2,moveybola=2;
+    ArrayList<Image> imgbrick=new ArrayList<>();
     public PanelBuatMain() {
-        initComponents();
+        initComponents();        
+        Image imgn=ballcon.getImage();
+        ball=imgn.getScaledInstance(15,15,java.awt.Image.SCALE_SMOOTH);
+        for (int i = 0; i < 10; i++) {
+            int rnd= (int)(Math.random()*5);
+            if (rnd==0) {
+                ImageIcon icon= new ImageIcon("brick.png");
+                Image img=icon.getImage();
+                Image newimg=img.getScaledInstance(50,25,java.awt.Image.SCALE_SMOOTH);                
+                imgbrick.add(newimg);
+            }
+            if (rnd==1) {
+                ImageIcon icon= new ImageIcon("brick1.png");
+                Image img=icon.getImage();
+                Image newimg=img.getScaledInstance(50,25,java.awt.Image.SCALE_SMOOTH);            
+                imgbrick.add(newimg);
+            }
+            if (rnd==2) {
+                ImageIcon icon= new ImageIcon("brick2.png");
+                Image img=icon.getImage();
+                Image newimg=img.getScaledInstance(50,25,java.awt.Image.SCALE_SMOOTH);
+                imgbrick.add(newimg);
+            }
+            if (rnd==3) {
+                ImageIcon icon= new ImageIcon("brick3.png");
+                Image img=icon.getImage();
+                Image newimg=img.getScaledInstance(50,25,java.awt.Image.SCALE_SMOOTH);
+                imgbrick.add(newimg);
+            }
+            if (rnd==4) {
+                ImageIcon icon= new ImageIcon("brick4.png");
+                Image img=icon.getImage();
+                Image newimg=img.getScaledInstance(50,25,java.awt.Image.SCALE_SMOOTH);
+                imgbrick.add(newimg);
+            }            
+        }
         try {
             ballbridge=ImageIO.read(new File("ball_bridge1.png"));
         } catch (IOException ex) {
             Logger.getLogger(From.class.getName()).log(Level.SEVERE, null, ex);
         }
         this.setFocusable(true);
+        Timer t =new Timer(10,new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                xbola+=movexbola;                
+                ybola+=moveybola;
+                repaint();
+            }
+        });
+        t.start();
     }
     BufferedImage ballbridge;
     @Override
@@ -41,6 +96,27 @@ public class PanelBuatMain extends javax.swing.JPanel {
                 Graphics2D g2 = (Graphics2D)grphcs;
         //posisi x,y,ukuran,ukuran,
         g2.drawImage(ballbridge,x,this.getHeight()-50,150,50,null);
+        for (int i = 0; i < imgbrick.size(); i++) {
+            g2.drawImage(imgbrick.get(i),(i*60)+50, 10, this);
+        }
+        g2.setColor(Color.blue);
+        if (xbola>this.getWidth()-30) {
+            movexbola*=-1;
+        }
+        if(ybola<0)
+        {
+            moveybola*=-1;
+        }
+        if (ybola>this.getHeight()-60&& xbola<=x+150 && xbola>=x) {
+            moveybola*=-1;
+        }
+        if (xbola<0) {
+            movexbola*=-1;
+        }
+                
+        g2.drawImage(ball, xbola,ybola, this);
+        
+        repaint();
     }
 
     @SuppressWarnings("unchecked")
